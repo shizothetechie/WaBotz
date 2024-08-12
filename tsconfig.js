ce5aacfab7d8c42c76dd903050d1a1999081666f
Shizo The Techie <98589136+shizothetechie@users.noreply.github.com>
2024-08-12 09:23:10 +0530

    Create tsconfig.js

---
+++ tsconfig.js
@@ -0,0 +1,17 @@
+{
+  "compilerOptions": {
+    "target": "ES2020",
+    "module": "ESNext",
+    "outDir": "./Xslab",
+    "strict": true,
+    "esModuleInterop": true,
+    "skipLibCheck": true,
+    "moduleResolution": "Node",
+    "declaration": true,
+    "declarationMap": true,
+    "sourceMap": true,
+    "allowSyntheticDefaultImports": true
+  },
+  "include": ["src/**/*.ts"],
+  "exclude": ["node_modules", "Xslab"]
+}
